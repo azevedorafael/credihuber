a3378290e7279d98f7e55e36618b1c9350d8fdee
azevedorafael <rafaelsilva.fatec@gmail.com>
2019-10-28 23:25:35 -0300

    Configure eslint

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,13 @@
-import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 
+import SignIn from './pages/SignIn';
 import Main from './pages/Main';
 import User from './pages/User';
 
 const Routes = createAppContainer(
-  createStackNavigator(
+  createSwitchNavigator(
     {
+      SignIn,
       Main,
       User,
     },
