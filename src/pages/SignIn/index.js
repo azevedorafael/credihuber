0a677c339b08f1aeb868a249aeaecac1422e1b38
azevedorafael <rafaelsilva.fatec@gmail.com>
2019-10-29 23:38:54 -0300

    Add Input + Button

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import { Text } from 'react-native';
 
+import Input from '~/components/Input';
+import Button from '~/components/Button';
+
 // import { Container } from './styles';
 
 export default function SignIn() {
-  return <Text>SignIn</Text>;
+  return (
+    <>
+      <Text>SignIn</Text>
+      <Input
+        style={{ marginTop: 30 }}
+        icon="call"
+        placeholder="Your Github email address"
+      />
+
+      <Button>Next</Button>
+    </>
+  );
 }
