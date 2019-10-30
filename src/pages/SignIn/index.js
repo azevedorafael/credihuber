68e6481495f4c6524b9c25151c23f3da71cbf48b
azevedorafael <rafaelsilva.fatec@gmail.com>
2019-10-30 20:56:41 -0300

    Add ref inputs control

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Image } from 'react-native';
 
 import logo from '~/assets/logo.png';
@@ -6,6 +6,10 @@ import logo from '~/assets/logo.png';
 import { Container, Form, FormInput, SubmitButton } from './styles';
 
 export default function SignIn() {
+  const passwordRef = useRef();
+
+  function handleSubmit() {}
+
   return (
     <Container>
       <Image source={logo} style={{ width: 150, height: 150 }} />
@@ -17,11 +21,20 @@ export default function SignIn() {
           autoCorrect={false}
           autoCapitalize="none"
           placeholder="Your Github email address"
+          returnKeyType="next"
+          onSubmitEditing={() => passwordRef.current.focus()}
         />
 
-        <FormInput icon="lock-outline" secureTextEntry placeholder="Password" />
+        <FormInput
+          icon="lock-outline"
+          secureTextEntry
+          placeholder="Password"
+          ref={passwordRef}
+          returnKeyType="send"
+          onSubmitEditing={handleSubmit}
+        />
 
-        <SubmitButton onPress={() => {}}>Next</SubmitButton>
+        <SubmitButton onPress={handleSubmit}>Next</SubmitButton>
       </Form>
     </Container>
   );
