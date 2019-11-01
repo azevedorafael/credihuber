7148f5873853344fe8851c7fa57580c82ea123ce
azevedorafael <rafaelsilva.fatec@gmail.com>
2019-10-31 21:09:02 -0300

    Control inputs state + dispatch action

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,14 +1,25 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { Image } from 'react-native';
+import { useDispatch, useSelector } from 'react-redux';
 
 import logo from '~/assets/logo.png';
+
+import { signInRequest } from '~/store/modules/auth/actions';
 
 import { Container, Form, FormInput, SubmitButton } from './styles';
 
 export default function SignIn() {
+  const dispatch = useDispatch();
   const passwordRef = useRef();
 
-  function handleSubmit() {}
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const loading = useSelector(state => state.auth.loading);
+
+  function handleSubmit() {
+    dispatch(signInRequest(email, password));
+  }
 
   return (
     <Container>
@@ -23,6 +34,8 @@ export default function SignIn() {
           placeholder="Your Github email address"
           returnKeyType="next"
           onSubmitEditing={() => passwordRef.current.focus()}
+          value={email}
+          onChangeText={setEmail}
         />
 
         <FormInput
@@ -32,9 +45,13 @@ export default function SignIn() {
           ref={passwordRef}
           returnKeyType="send"
           onSubmitEditing={handleSubmit}
+          value={password}
+          onChangeText={setPassword}
         />
 
-        <SubmitButton onPress={handleSubmit}>Next</SubmitButton>
+        <SubmitButton loading={loading} onPress={handleSubmit}>
+          Next
+        </SubmitButton>
       </Form>
     </Container>
   );
