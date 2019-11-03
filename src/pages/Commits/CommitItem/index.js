c6801efbc9e476136972d6c0d4d5b0543d3d5e78
azevedorafael <rafaelsilva.fatec@gmail.com>
2019-11-03 18:14:51 -0300

    Fix avatar uri

--- src/pages/Commits/CommitItem/index.js
+++ src/pages/Commits/CommitItem/index.js
@@ -1,30 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { withNavigation } from 'react-navigation';
-
 import { MainContainer, CommitContainer, Text, OwnerAvatar } from './styles';
 
-const CommitItem = ({ commit }) => (
-  <MainContainer>
-    <OwnerAvatar source={{ uri: commit.commit.committer.avatar_url }} />
-    <CommitContainer>
-      <Text>{commit.commit.message}</Text>
-    </CommitContainer>
-  </MainContainer>
-);
+export default function CommitItem({ commit }) {
+  const avatar_url = commit.committer ? commit.committer.avatar_url : '';
+
+  return (
+    <MainContainer>
+      <OwnerAvatar source={{ uri: avatar_url }} />
+      <CommitContainer>
+        <Text>{commit.commit.message}</Text>
+      </CommitContainer>
+    </MainContainer>
+  );
+}
 
 CommitItem.propTypes = {
   commit: PropTypes.shape({
-    message: PropTypes.string,
     commit: PropTypes.shape({
-      commit: PropTypes.string,
       message: PropTypes.string,
-      committer: PropTypes.shape({
-        avatar_url: PropTypes.string,
-      }),
+    }),
+    committer: PropTypes.shape({
+      avatar_url: PropTypes.string,
     }),
   }).isRequired,
 };
-
-export default withNavigation(CommitItem);
